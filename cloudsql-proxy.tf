36d5f10493787db07a26eb50da10b51c18640c50
daylight55 <jojomasanori3@gmail.com>
2023-06-01 01:49:42 +0900

    feat: 🎸 cloudsql-proxy定義を外部moduleを使用して定義

---
+++ cloudsql-proxy.tf
@@ -0,0 +1,18 @@
+module "cloudsql-proxy" {
+  source  = "appchoose/cloudsql-proxy/google"
+  version = "0.1.0"
+
+  project         = var.project_name
+  container_image = "eu.gcr.io/cloudsql-docker/gce-proxy:1.32.0"
+  instance_name   = "cloudsql-proxy"
+  container_args = [
+    "-instances=${google_sql_database_instance.instance.connection_name}=tcp:0.0.0.0:5432",
+    "-enable_iam_login",
+  ]
+
+  vm_zone       = var.zone
+  vm_subnetwork = google_compute_subnetwork.private_network["private-instance-subnet"].self_link
+
+  firewall_network       = google_compute_network.vpc.self_link
+  firewall_source_ranges = ["0.0.0.0/0"]
+}
