b0101031893d530e1eabfd1779b12e06188d0cd8
daylight55 <jojomasanori3@gmail.com>
2023-06-02 08:10:34 +0900

    feat: 🎸 デフォルトルートの削除追加とVPCピアリングのリソース名修正

--- network.tf
+++ network.tf
@@ -22,10 +22,11 @@ resource "google_compute_network" "vpc" {
 }
 
 resource "google_compute_network" "private_network" {
-  name = "${local.prefix}-private-vpc"
-  # auto_create_subnetworks = false
-  routing_mode = "REGIONAL"
-  mtu          = 1460
+  name                            = "${local.prefix}-private-vpc"
+  auto_create_subnetworks         = false
+  routing_mode                    = "REGIONAL"
+  mtu                             = 1460
+  delete_default_routes_on_create = true // IGWルーティングなどのデフォルトルートを削除する
 }
 
 resource "google_compute_subnetwork" "public" {
@@ -41,11 +42,11 @@ resource "google_compute_subnetwork" "private" {
 
   name          = "${local.prefix}-${each.key}"
   ip_cidr_range = each.value.cidr
-  network       = google_compute_network.vpc.id
+  network       = google_compute_network.private_network.id
 }
 
-resource "google_compute_global_address" "db_private_ip_address" {
-  name          = "db-private-ip-address"
+resource "google_compute_global_address" "private_ip_address_allocated_for_private_vpc_connection" {
+  name          = "private-ip-address-allocated-for-private-vpc-connection"
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
   prefix_length = 16
@@ -55,5 +56,5 @@ resource "google_compute_global_address" "db_private_ip_address" {
 resource "google_service_networking_connection" "private_vpc_connection" {
   network                 = google_compute_network.private_network.id
   service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = [google_compute_global_address.db_private_ip_address.name]
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address_allocated_for_private_vpc_connection.name]
 }
