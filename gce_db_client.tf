1fd84763fec29530e88e0bed811816b6c8227a10
daylight55 <jojomasanori3@gmail.com>
2023-06-01 09:41:59 +0900

    feat: 🎸 DBクライアントの初期起動コマンドを修正

--- gce_db_client.tf
+++ gce_db_client.tf
@@ -1,37 +1,45 @@
+resource "google_service_account" "db_client" {
+  account_id   = "sa-${local.db_client_instance_name}"
+  display_name = "DB Client sa for VM ${local.db_client_instance_name}"
+  project      = var.project_name
+  description  = "Used by the VM ${local.db_client_instance_name}"
+}
+
 resource "google_compute_instance" "db_client" {
-  name         = local.db_client_instance_name
-  machine_type = "e2-micro"
-  zone         = var.zone
+  name                      = local.db_client_instance_name
+  machine_type              = "e2-micro"
+  zone                      = var.zone
+  allow_stopping_for_update = true // インスタンス更新時の停止を許可する
+
+  # scheduling {
+  #   automatic_restart = true
+  # }
 
   tags = [local.db_client_instance_name]
 
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
-  }
-
-  // Local SSD disk
-  scratch_disk {
-    interface = "SCSI"
+    }
   }
 
   network_interface {
-    network = "default"
+    network = google_compute_network.private_network.id
+    # subnetwork = google_compute_subnetwork.private["private-instance-subnet"].id
 
-    access_config {
-      // Ephemeral public IP
-    }
+    // Ephemeral public IP
+    access_config {}
   }
 
   metadata_startup_script = <<-EOT
   sudo apt-get update -y && \
+  sudo apt install locales-all -y && \
   sudo apt-get install postgresql-client -y
   EOT
 
-
   service_account {
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    email  = google_service_account.default.email
+    email  = google_service_account.db_client.email
     scopes = ["cloud-platform"]
   }
 }
